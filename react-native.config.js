f6f6fd3513583fa4d16c3bb9236f384d073dbc21
AlexDeveloper12 <angrycoder95@gmail.com>
2023-10-19 23:30:37 +0100

    Adding fonts

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    dependencies: {
-      'react-native-vector-icons': {
-        platforms: {
-          ios: null,
-        },
+  project: {
+    ios: {},
+    android: {}
+  },
+  assets: ['./assets/fonts/'],
+  
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
       },
     },
-  };
+  },
+};
