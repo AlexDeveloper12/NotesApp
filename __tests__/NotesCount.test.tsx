2f8a78bfcba88b9004ee74cae07b9ebde65761d4
AlexDeveloper12 <angrycoder95@gmail.com>
2023-11-08 19:44:06 +0000

    Changes

--- __tests__/NotesCount.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import NotesCount from "../src/components/NotesCount/NotesCount"
-import renderer from 'react-test-renderer';
-
-test('loads and displays note count', async () => {
-    const tree = renderer.create(<NotesCount count={5}/>).toJSON();
-    expect(tree).toMatchSnapshot();
-});
