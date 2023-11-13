fbf8105c98147d0e10ea1aa0f17840386821228e
AlexDeveloper12 <angrycoder95@gmail.com>
2023-11-13 22:35:54 +0000

    Removing unnecessary test

--- __tests__/SortAscending.test.tsx
+++ __tests__/SortAscending.test.tsx
@@ -29,28 +29,29 @@ describe('SortAscending', () => {
 
         const result = SortAscending(testArray);
 
-        const expected = [{
-            id: 4,
-            noteText: 'Fourth value',
-            isFavourite: false,
-            dateCreated: "2023-10-31T00:05:48.523Z"
-        }, {
-            id: 1,
-            noteText: 'First value',
-            isFavourite: false,
-            dateCreated: "2023-10-31T00:07:48.523Z"
-        }, {
-            id: 2,
-            noteText: 'Second value',
-            isFavourite: false,
-            dateCreated: "2023-10-31T00:09:48.523Z"
-        },
-        {
-            id: 3,
-            noteText: 'Third value',
-            isFavourite: false,
-            dateCreated: "2023-10-31T00:11:48.523Z"
-        },
+        const expected = [
+            {
+                id: 1,
+                noteText: 'First value',
+                isFavourite: false,
+                dateCreated: "2023-10-31T00:07:48.523Z"
+            }, {
+                id: 4,
+                noteText: 'Fourth value',
+                isFavourite: false,
+                dateCreated: "2023-10-31T00:05:48.523Z"
+            }, {
+                id: 2,
+                noteText: 'Second value',
+                isFavourite: false,
+                dateCreated: "2023-10-31T00:09:48.523Z"
+            },
+            {
+                id: 3,
+                noteText: 'Third value',
+                isFavourite: false,
+                dateCreated: "2023-10-31T00:11:48.523Z"
+            },
         ];
 
         expect(result).toEqual(expected);
--- __tests__/SortDescending.test.tsx
+++ __tests__/SortDescending.test.tsx
@@ -38,18 +38,19 @@ describe('SortDescending', () => {
             noteText: 'Second value',
             isFavourite: false,
             dateCreated: "2023-10-31T00:09:48.523Z"
-        }, {
-            id: 1,
-            noteText: 'First value',
-            isFavourite: false,
-            dateCreated: "2023-10-31T00:07:48.523Z"
         },
         {
             id: 4,
             noteText: 'Fourth value',
             isFavourite: false,
             dateCreated: "2023-10-31T00:05:48.523Z"
-        }];
+        }, {
+            id: 1,
+            noteText: 'First value',
+            isFavourite: false,
+            dateCreated: "2023-10-31T00:07:48.523Z"
+        },
+        ];
 
         expect(result).toEqual(expected);
     });
